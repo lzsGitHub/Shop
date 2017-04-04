cc04bd3d6e960bf2ae29bba1db56c43b2653307f
Sixteen_Night <787534698@qq.com>
2017-04-04 11:43:55 +0800

    git@github.com:lzsGitHub/code.git

--- thx播放器/src/main/java/com/example/shop/thx/Main2Activity.java
+++ thx播放器/src/main/java/com/example/shop/thx/Main2Activity.java
@@ -10,6 +10,6 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         System.out.println("sdfsd");
-        System.out.println("asda");
+        System.out.println("asda"); System.out.println("asda");
     }
 }
